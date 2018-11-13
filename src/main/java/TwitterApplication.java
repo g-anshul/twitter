3c415bc3f28f28e5ae5a1ae1c38cec468ee7a627
Anshul Gupta <anshul.gupta@lithium.com>
2018-11-13 14:26:49 +0530

    basic package structure created

---
+++ src/main/java/TwitterApplication.java
@@ -0,0 +1,7 @@
+
+
+/**
+ * Created by anshul.gupta on 11/13/18.
+ */
+public class TwitterApplication extends   {
+}
---
+++ src/main/java/com/restapi/Endpoint.java
@@ -0,0 +1,7 @@
+package com.restapi;
+
+/**
+ * Created by anshul.gupta on 11/13/18.
+ */
+public class Endpoint {
+}
